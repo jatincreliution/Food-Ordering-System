02f9034a8017181520ff7b63da5b9619e2eb329c
jatincreliution <jatinc.reliution@gmail.com>
2025-08-20 15:14:46 +0530

    Update food_main.py

--- food_main.py
+++ food_main.py
@@ -4,9 +4,12 @@
 Created on Sun Jun 26 08:46:19 2016
 @author: roshni
 """
+
 import sqlite3
 import pandas as pd
 import re
+import sys
+
 def login():
     username = input(" Enter Email ID : ")  
     password = input(" Enter Password : ")
@@ -17,10 +20,8 @@ def login():
         for element in tup:
             cust_id = str(element)
             
-    
     if(len(lst)>0):
         login=open('login.txt','w')
-        
         login.write(cust_id)
         login.close()
         while True:
@@ -68,82 +69,84 @@ def show_menu():
     print("======| MENU FOR DIFFERENT SHOPS |======\n" )
     print("     ======| GOLDEN GATE |======\n" )    
     print(pd.read_sql_query("select item_id,item_name,item_price from shop_menu where shop_id=1", conn))
-    print("\n     ======| BELLAGIO |======\n" )	
+    print("\n     ======| BELLAGIO |======\n" )    
     print(pd.read_sql_query("select item_id,item_name,item_price from shop_menu where shop_id=2", conn))
     print("\n     ======| BAWARCHI |======\n" )
     print(pd.read_sql_query("select item_id,item_name,item_price from shop_menu where shop_id=3", conn))
+
 def new_user():
-        print("======| Please enter the Customer Details |======")
-        
-        cust_fname = input(" First Name (only 15 characters): ")
-        while (len(cust_fname)>15) or not re.match("^[a-zA-z]*$",cust_fname):
-            print("Error! Only 15 characters a-z allowed.")
-            cust_fname = input(" First Name : ")
+    print("======| Please enter the Customer Details |======")
     
-        cust_lname = input(" Last Name (only 15 characters): ")
-        while (len(cust_lname)>15) or not re.match("^[a-zA-z]*$",cust_lname):
-            print("Error! Only 15 characters a-z allowed.")
-            cust_lname = input(" Last Name : ")
-        cust_address = input(" Address : ")
-        cust_pincode = input(" Pincode(maximun 10 characters) : ")
-        while (len(cust_pincode)>10) or not re.match("^[0-9]*$",cust_pincode):
-            print("Error! Only 10 digits allowed")
-            cust_pincode = int(input(" Pincode : "))
-        cust_mobileno = input(" Mobile Number(10 digits) : ")
+    cust_fname = input(" First Name (only 15 characters): ")
+    while (len(cust_fname)>15) or not re.match("^[a-zA-Z]*$",cust_fname):
+        print("Error! Only 15 characters a-z allowed.")
+        cust_fname = input(" First Name : ")
+
+    cust_lname = input(" Last Name (only 15 characters): ")
+    while (len(cust_lname)>15) or not re.match("^[a-zA-Z]*$",cust_lname):
+        print("Error! Only 15 characters a-z allowed.")
+        cust_lname = input(" Last Name : ")
+    cust_address = input(" Address : ")
+    cust_pincode = input(" Pincode(maximum 10 characters) : ")
+    while (len(cust_pincode)>10) or not re.match("^[0-9]*$",cust_pincode):
+        print("Error! Only 10 digits allowed")
+        cust_pincode = input(" Pincode : ")
+    cust_mobileno = input(" Mobile Number(10 digits) : ")
+
+    while (len(cust_mobileno)!=10):
+        print("Error! Only 10 digits 0-9 allowed.")
+        cust_mobileno = input(" Mobile Number :")
+    email_id = input(" Email ID : ")
+    if not re.match("[a-z0-9]\\S*@\\S*[a-z]", email_id):
+        print("ERROR..!!! Not a Valid Email-id")
+        email_id = input("enter a valid Email ID:") 
+    cust_password = input(" Password :")
+    curs = conn.cursor()
+    curs.execute("INSERT INTO customer_master (cust_fname, cust_lname, cust_address, cust_pincode, mobileno,email_id,password) values (?,?,?,?,?,?,?)",
+                 (cust_fname, cust_lname, cust_address, cust_pincode, cust_mobileno,email_id,cust_password))
+    print("======| NEW CUSTOMER ADDED SUCCESSFULLY |======\n")
     
-        while (len(cust_mobileno)!=10):
-            print("Error! Only 10 digits 0-9 allowed.")
-            cust_mobileno = input(" Mobie Number :")
-        email_id = input(" Email ID : ")
-        if not re.match("[a-z0-9]\S*@\S*[a-z]", email_id):
-            print("ERROR..!!! Not a Valid Email-id")
-            email_id = input("enter a valid Email ID:") 
-        cust_password = input(" Password :")
-        curs = conn.cursor()
-        curs.execute("INSERT INTO customer_master (cust_fname, cust_lname, cust_address, cust_pincode, mobileno,email_id,password) values (?,?,?,?,?,?,?)",
-                     (cust_fname, cust_lname, cust_address, cust_pincode, cust_mobileno,email_id,cust_password))
-        print("======| NEW CUSTOMER ADDED SUCCESSFULLY |======\n")
-        
-        conn.commit()
+    conn.commit()
+
 def info():
     print("WE ARE AWESOME !!")
-	
-													
-		
 
 
-
-
-## this is the main program
 
 conn = sqlite3.connect('shop_master_db')
 
+# Non-interactive mode for Jenkins CI
+if '--ci' in sys.argv:
+    print("Running in CI mode: showing menu only (no input)...\n")
+    show_menu()
+    conn.close()
+    sys.exit(0)
+
+# Normal interactive mode
 while True:
-	print('=================================\n')
-	print('  WELCOME TO HII HUNGRY  \n')
-	print('=================================\n')
-	print(' Please choose from the following option : ')
-	print()
-	print('1. NEW USER \n')
-	print('2. LOGIN \n')
-	print('3. SHOW MENU \n')
-	print('4. ABOUT US \n')
-	
-	print('0. CLOSE \n')
+    print('=================================\n')
+    print('  WELCOME TO HII HUNGRY  \n')
+    print('=================================\n')
+    print(' Please choose from the following option : ')
+    print()
+    print('1. NEW USER \n')
+    print('2. LOGIN \n')
+    print('3. SHOW MENU \n')
+    print('4. ABOUT US \n')
+    print('0. CLOSE \n')
 
-	choice = (input("Please Enter Your Option : "))
-	if choice == '1':
-		new_user()
-	elif choice == '2':
-		login()
-	elif choice == '3':
-		show_menu()
-	elif choice == '4':
-		info()
-	elif choice == '0':
-		conn.close()
-		break
-	else:
-		print("Invalid choice ....!!!!\n")
-		print("Please enter a valid option")
-
+    choice = (input("Please Enter Your Option : "))
+    if choice == '1':
+        new_user()
+    elif choice == '2':
+        login()
+    elif choice == '3':
+        show_menu()
+    elif choice == '4':
+        info()
+    elif choice == '0':
+        conn.close()
+        break
+    else:
+        print("Invalid choice ....!!!!\n")
+        print("Please enter a valid option")
